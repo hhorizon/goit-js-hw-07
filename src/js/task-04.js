0617346d0b09bd5c4e70ccb27e7b05ddd79b2a44
Anatolii Litynskyi <litinskiy.tolik@gmail.com>
2021-09-19 18:50:36 +0300

    done

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,12 +1,10 @@
 const counterValue = document.querySelector('#value');
+
 const incrementBtn = document.querySelector('[data-action="increment"]');
-const decrementBtn = document.querySelector('[data-action="decrement"]');
-
 const increment = () => counterValue.textContent++;
-
-const decrement = () => counterValue.textContent--;
-
-
 incrementBtn.addEventListener('click', increment);
+
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+const decrement = () => counterValue.textContent--;
 decrementBtn.addEventListener('click', decrement);
 
